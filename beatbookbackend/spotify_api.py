1e340ea1bd508e2bbfbb4c389c383b4514f9dc21
Claire Hackl <chackl@nd.edu>
2023-10-26 10:52:27 -0400

    Add venv and working user added to mysql database

---
+++ beatbookbackend/spotify_api.py
@@ -0,0 +1,25 @@
+import requests
+import pprint
+
+def get_user(mysql, headers):
+    # Request user's profile from the spotify API
+    r = requests.get(url='https://api.spotify.com/v1/me', headers=headers)
+
+    # Data from the request
+    username = r.json()['id']
+    display_name = r.json()['display_name']
+
+    # Select the usernames from the database
+    cursor = mysql.connection.cursor()
+    cursor.execute("select username from Users");
+    usernames = [row[0] for row in cursor.fetchall()]
+
+    # If the user does not already exist, add to the database
+    if username not in usernames:
+        cursor.execute("insert into Users (username, display_name) values (%s, %s)", 
+            (username, display_name));
+        cursor.connection.commit()
+
+    cursor.close()
+
+    return username
