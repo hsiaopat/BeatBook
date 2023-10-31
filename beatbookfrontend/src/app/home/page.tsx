62e034b6b2a92404382f796e1ec39358a503882e
Ellie Jensen <elliejensen@ellies-air.dhcp.nd.edu>
2023-10-31 16:51:11 -0400

    spotify log in button

--- beatbookfrontend/src/app/home/page.tsx
+++ beatbookfrontend/src/app/home/page.tsx
@@ -1,71 +1,80 @@
-/* import Link from 'next/link';
+'use client'
+import Link from 'next/link';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import { styled } from '@mui/system'
+import { styled } from '@mui/system';
 
-const containerStyles = {
+const Container = styled('div')({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'flex-start',
   alignItems: 'center',
   height: '100vh',
-};
+});
 
-const whiteBackgroundStyles = {
+const WhiteBackground = styled('div')({
   background: 'white',
   width: '100%',
   borderBottom: '1px solid black',
-};
+});
 
-const titleStyles = {
+const Title = styled('h1')({
   fontSize: '2rem',
   color: 'black',
   margin: 0,
   textAlign: 'center',
   padding: '1rem 0',
-};
+});
 
-const centeredButtonStyles = {
+const CenteredButtons = styled('div')({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
   flex: 1,
-};
+});
 
-const greenButtonStyles = {
-  backgroundColor: 'forestgreen', // Set the background color to green
-  color: 'black', // Set the text color to black
+const GreenButton = styled(Button)({
+  backgroundColor: 'forestgreen',
+  color: 'black',
   fontSize: '1rem',
   margin: '1rem 0',
-  '&:hover': {
-    backgroundColor: 'forestgreen', // Set the background color to green on hover
-  },
-};
+});
+
+const SpotifyLoginButton = styled(Button)({
+  backgroundColor: 'lightblue', // Customize the background color for the Spotify login button
+  color: 'black', // Text color
+  fontSize: '1rem',
+  margin: '1rem 0',
+});
 
 const HomePage = () => {
+  const redirectToSpotify = () => {
+    // Save the user ID or perform any other necessary actions
+    const userId = 'your-user-id'; // Replace with actual user ID
+    // Redirect to the Spotify login page
+    window.location.href = 'https://accounts.spotify.com';
+  };
+
   return (
-    <Box sx={containerStyles}>
-      <Box sx={whiteBackgroundStyles}>
-        <Box sx={titleStyles}>BeatBook</Box>
-      </Box>
-      <Box sx={centeredButtonStyles}>
+    <Container>
+      <WhiteBackground>
+        <Title>BeatBook</Title>
+      </WhiteBackground>
+      <CenteredButtons>
+        <SpotifyLoginButton variant="contained" onClick={redirectToSpotify}>
+          Log in to Spotify
+        </SpotifyLoginButton>
         <Link href="/group-login">
-          <Button sx={greenButtonStyles} variant="contained">
-            Group Login
-          </Button>
+          <GreenButton variant="contained">Group Login</GreenButton>
         </Link>
-      </Box>
-    </Box>
+      </CenteredButtons>
+    </Container>
   );
 };
 
 export default HomePage;
 
-*/
-
-
-'use client'
+/* 'use client'
 import Link from 'next/link';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/system';
@@ -115,7 +124,7 @@ const HomePage = () => {
         <Title>BeatBook</Title>
       </WhiteBackground>
       <CenteredButton>
-        <Link href="/group-login">
+        <Link href="">
           <GreenButton variant="contained">Group Login</GreenButton>
         </Link>
       </CenteredButton>
@@ -124,3 +133,5 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+*/
