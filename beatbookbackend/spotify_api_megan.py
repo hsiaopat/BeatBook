7277bd5996ae72b7bd5b35714bf08d42fc60d9c9
Claire Hackl <chackl@nd.edu>
2023-10-31 20:23:47 -0400

    draft of group functions

--- beatbookbackend/spotify_api_megan.py
+++ beatbookbackend/spotify_api_megan.py
@@ -37,7 +37,6 @@ def get_user_top_tracks(mysql, headers):
 
     tracks = []
     tracks_id = []
-    #for each song add in its name, id, artist id, artist, album, duration and popularity (0-100)
     for item in data['items']:
         track_name = item['name']
         tracks.append(track_name)
@@ -98,23 +97,21 @@ def get_user_top_artists(mysql, headers):
 
     return artists, artists_id
 
-#need to input the artist id
+
 def get_artists_genre(mysql, headers,id):
     url = 'https://api.spotify.com/v1/artists/{0}/'.format(id)
     params = {
         
     }
-    #list of artist genres
+    
     data = requests.get(url=url, params=params, headers=headers).json()
     genre = data['genres']
     return genre
 
 def get_user_stats(mysql, headers):
-    #User_ID
     username = get_user(mysql, headers)
-    
+    print(username)
     tracks, track_ids = get_user_top_tracks(mysql,headers)
-    #select the top artists from the top tracks db
     cursor = mysql.connection.cursor()
     cursor.execute("select Artist_ID from Tracks");
     artist_id = [row[0] for row in cursor.fetchall()]
@@ -123,7 +120,6 @@ def get_user_stats(mysql, headers):
     command = "select display_name from Users where username = '%s'" % username
     cursor.execute(command)
     name = str([row[0] for row in cursor.fetchall()])
-    #get current top track 
     top_track = tracks[0]
     top_track_id = track_ids[0]
     genres = []
@@ -134,24 +130,23 @@ def get_user_stats(mysql, headers):
         gen = get_artists_genre(mysql,headers,art_id)
         genres.extend(gen)
         art.append(art_name)
-    #Count the number of each genre and artist and take the most common
     count = Counter(genres)
     count_artist = Counter(art)
     top_artist = count_artist.most_common(1)[0][0]
     top_genre = count.most_common(1)[0][0]
-    # Select the username from the database
+    # Select the artist id from the database
 
     cursor = mysql.connection.cursor()
     cursor.execute("select username from User_Stats");
     users_id = [row[0] for row in cursor.fetchall()]
 
-    # If the username  does not already exist, add to the database
+    # If the artist id does not already exist, add to the database
     if username not in users_id:
         cursor.execute("insert into User_Stats (username, name, top_track_id, top_track, top_artist, top_genre) values (%s, %s, %s, %s, %s, %s)",         
             (username, name, top_track_id, top_track, top_artist, top_genre));
         cursor.connection.commit()
         cursor.close()
-    #if the username does exist, update their current stats
+
     elif username in users_id:
         command = "update User_Stats set top_track_id = '%s', top_track = '%s', top_artist = '%s', top_genre = '%s' where username = '%s'" % (top_track_id, top_track, top_artist, top_genre, username)
         print(command)
@@ -159,3 +154,75 @@ def get_user_stats(mysql, headers):
         cursor.connection.commit()
         cursor.close()
 
+#NOT IMPLEMENTED YET OR TESTED
+def join_group(mysql,headers, g_id):
+    cursor = mysql.connection.cursor()
+    username = get_user(mysql, headers)
+    command = "select display_name from Users where username = '%s'" % username
+    cursor.execute(command)
+    name = [row[0] for row in cursor.fetchall()]
+    cursor.execute("select group_id from Clubs")
+    groups_ids = [row[0] for row in cursor.fetchall()]
+    cursor.execute("show tables")
+    tables = [row[0] for row in cursor.fetchall()]
+    group_name_id = 'Group_%s' % g_id
+    if (g_id in groups_ids and group_name_id in tables):
+        command = "select Member_username from %s" % group_name_id
+        cursor.execute(command)
+        current_members = [row[0] for row in cursor.fetchall()]
+        if username not in current_members:
+            cursor.execute("insert into Group_%s (Member_username, Member_name) values (%s, %s)",
+                (g_id, username, name))
+            cursor.execute("update Clubs set num_members = num_members+1 where group_id = '%s'" , g_id)
+            cursor.connection.commit()
+            cursor.close()
+        else:
+            print("User already in group")
+    else:
+        print("Group does not exist. You must first create the group")
+
+#NOT IMPLEMENTED OR TESTED
+def create_group(mysql, headers, group_name):
+    username = get_user(mysql, headers)
+    cursor.mysql.connection.cursor()
+    command = "select display_name from Users where username = '%s'" % username
+    cursor.execute(command)
+    name = [row[0] for row in cursor.fetchall()]
+    cursor.execute("select group_id from Clubs")
+    groups_ids = [row[0] for row in cursor.fetchall()]
+    cursor.execute("select group_id from Clubs order by group_id desc limit 1")
+    new_id = [row[0] for row in cursor.fetchall()]+1
+    while new_id in groups_ids:
+        new_id = new_id+1
+    if new_id not in groups_ids:
+        cursor.execute("insert into Clubs (group_id, group_name, num_members) values (%s, %s, 1)",
+            (new_id, group_name))
+        command = "create table Group_%s(Member_username varchar(50) primary key, Member_name varchar(100)" % new_id
+        cursor.execute(command)
+        command = "insert into Group_%s (Member_username, Member_name) values (%s, %s)" % (username, name)
+        cursor.execute(command)
+        cursor.connection.commit()
+        cursor.close()
+
+
+#NOT IMPLEMENTED OR TESTED
+def leave_group(mysql, headers, group_id):
+    username = get_user(mysql, headers)
+    cursor.mysql.connection.cursor()
+    group_name_id = "Group_%s" % group_id
+    cursor.execute("show tables")
+    tables = [row[0] for row in cursor.fetchall()]
+    if group_name_id in tables:
+        command = "select Member_username from Group_%s" % group_id
+        cursor.execute(command)
+        current_members = [row[0] for row in cursor.fetchall()]
+        if username in current_members:
+            #USES DELETE NEED TO EVALUATE
+            command = "delete from Group_%s where Member_username = '%s" % (group_id, username)
+            cursor.execute(command)
+            cursor.connection.commit()
+            cursor.close()
+#NOT IMPLEMENTED OR TESTED
+#NEED TO FIGURE OUT WHO CAN DELETE GROUPS
+def delete_group(mysql, headers):
+    print("Help")
